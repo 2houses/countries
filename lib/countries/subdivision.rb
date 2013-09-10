0f59b3ded28a0731a4cf6abaab43ec041dda7fbd
cedric <cedric@studio-sept.be>
2013-09-10 11:08:40 +0200

    Improve translation methods

--- lib/countries/subdivision.rb
+++ lib/countries/subdivision.rb
@@ -2,9 +2,16 @@ module ISO3166; end
 
 class ISO3166::Subdivision
 
-  def initialize(subdivision)
+  def initialize(subdivision, iso3)
     @subdivision = subdivision
+    @iso3 = iso3
   end
+
+  def iso3
+    @iso3
+  end
+
+  alias :code :iso3
 
   def name
     @subdivision['name']
--- lib/countries/subdivisions.rb
+++ lib/countries/subdivisions.rb
@@ -13,10 +13,34 @@ class ISO3166::Subdivisions
     end
   end
 
+  def all
+    @subdivisions
+  end
+
   def find(iso3)
-    Subdivision.new(@subdivisions[iso3])
+    Subdivision.new(@subdivisions[iso3], iso3)
   end
 
   alias :[] :find
+
+  def find_by_name(name)
+    subdivision = all.detect{|sub| sub[1]["name"] == name }
+    find(subdivision[0])
+  end
+
+  def find_by_parameterized_name(parameterized_name)
+    subdivision = all.detect{|sub| sub[1]["name"].parameterize == parameterized_name }
+    find(subdivision[0])
+  end
+
+  def find_by_translation(translated_name)
+    subdivision = all.detect{|sub| sub[1]['translations'].has_value?(translated_name) }
+    find(subdivision[0])
+  end
+
+  def find_by_parameterized_translation(parameterized_translated_name)
+    subdivision = all.detect{|sub| sub[1]['translations'].detect{ |sub2| sub2[1].parameterize == parameterized_translated_name } }
+    find(subdivision[0])
+  end
 
 end
